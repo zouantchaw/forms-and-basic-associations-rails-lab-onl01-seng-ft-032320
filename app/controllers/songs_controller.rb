2e0fad8d6ea113a372852b58e2264c1ffd39ca3d
Steven G. Harms <sgharms@stevengharms.com>
2019-10-31 07:48:42 -0400

    wip-sol

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,13 +9,20 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+
+    # Build 3 "empty" notes that are automatically set up with the correct
+    # parameter names to be used by accepts_nested_attributes_for
+    # See:
+    # https://api.rubyonrails.org/v5.2.3/classes/ActionView/Helpers/FormHelper.html#method-i-fields_for
+    3.times { @song.notes.build }
   end
 
   def create
-    @song = Song.new(song_params)
+    artist = Artist.find_or_create_by(name: song_params[:artist_name])
+    @song = artist.songs.build(song_params)
 
     if @song.save
-      redirect_to @song
+      redirect_to songs_path
     else
       render :new
     end
@@ -47,7 +54,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :genre_id, :artist_name, notes_attributes: [:content])
   end
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,22 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  # This is some high-power Rails magic
+  # accepts_nested_attributes_for lets us create multiple Notes when creating a
+  # Song. That's pretty awesome. However we can define a proc (a small in-line
+  # method) that is passed the attributes that are about to be handed to the
+  # to-be-created Note and cancel the operation (reject_if) if they are judged
+  # to be invalid (i.e. the content attribute is empty). Soooo...powerful!
+  accepts_nested_attributes_for :notes, :reject_if => proc { |attrs| attrs[:content].blank? }
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    artist.try(:name)
+  end
 end
+
